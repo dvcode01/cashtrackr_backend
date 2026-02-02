afd10188f8affa3b7785a137f8c4c8505e575ae8
David <daviolivero2103@gmail.com>
2026-02-02 14:55:42 -0400

    feat(models): create budget model

---
+++ src/models/Budget.ts
@@ -0,0 +1,21 @@
+import { Column, DataType, HasMany, Table, ForeignKey, BelongsTo, Model } from 'sequelize-typescript';
+
+@Table({
+    tableName: 'budgets'
+})
+
+class Budget extends Model {
+    @Column({
+        type: DataType.STRING(100)
+    })
+
+    name: string;
+
+    @Column({
+        type: DataType.DECIMAL
+    })
+
+    amount: number;
+}
+
+export default Budget;
