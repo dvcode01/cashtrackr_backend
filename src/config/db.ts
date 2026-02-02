d8c1f51171d7408840f6bc032cede4592aa6b3e5
David <daviolivero2103@gmail.com>
2026-02-02 14:55:07 -0400

    chore(config): add configuration for models

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,10 +4,12 @@ import { Sequelize } from "sequelize-typescript";
 dotenv.config();
 
 export const db = new Sequelize(process.env.DATABASE_URL, {
+    models: [__dirname + '/../models/**/*'],
     dialectOptions: {
         ssl: {
             require: false
         }
     },
+    logging: false,
     dialect: 'postgres'
 });
