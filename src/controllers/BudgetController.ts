3781401e549f3ebc6ffa79c2bfc9a455b7c7bddf
David <daviolivero2103@gmail.com>
2026-02-05 15:32:15 -0400

    feat(controllers): show expenses that belong to a budget

--- src/controllers/BudgetController.ts
+++ src/controllers/BudgetController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Budget from "../models/Budget";
+import Expense from "../models/Expense";
 
 export class BudgetController {
 
@@ -33,7 +34,11 @@ export class BudgetController {
     }
 
     public static getById = async (req: Request, res: Response) => {
-        res.json(req.budget)
+        const budget = await Budget.findByPk(req.budget.id, {
+            include: [Expense]
+        });
+
+        res.json(budget);
     }
 
     public static updateById = async (req: Request, res: Response) => {
