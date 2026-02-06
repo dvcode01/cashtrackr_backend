7277d8999092afb2c63759c368946e40ae777f4a
David <daviolivero2103@gmail.com>
2026-02-06 12:25:27 -0400

    feat(middlewares): validate that the expense exists and its ID

--- src/middlewares/Expense.ts
+++ src/middlewares/Expense.ts
@@ -1,5 +1,14 @@
 import type { Request, Response, NextFunction } from 'express';
-import { body } from 'express-validator';
+import { body, param, validationResult } from 'express-validator';
+import Expense from '../models/Expense';
+
+declare global {
+    namespace Express {
+        interface Request {
+            expense?: Expense
+        }
+    }
+}
 
 export const validateExpenseInput = async (req: Request, res: Response, next: NextFunction) => {
     await body('name')
@@ -11,3 +20,36 @@ export const validateExpenseInput = async (req: Request, res: Response, next: Ne
 
     next();
 }
+
+export const validateExpenseById = async (req: Request, res: Response, next: NextFunction) => {
+
+    await param('expenseID').isInt().custom(value => value > 0)
+            .withMessage('Invalid ID')
+            .run(req)
+
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    next();
+};
+
+export const validateExpenseExist = async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const { expenseID } = req.params
+        const expense = await Expense.findOne({where: {id: expenseID}});
+        
+        if(!expense){
+            const error = new Error('Expense not found');
+            return res.status(404).json({error: error.message});
+        }
+
+        req.expense = expense;
+        next();
+    } catch (error) {
+        res.status(500).json({error: 'There was a mistake'});
+    }
+};
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -3,12 +3,15 @@ import { BudgetController } from "../controllers/BudgetController";
 import { handleInputErrors } from "../middlewares/validation";
 import { validateBudgetById, validateBudgetExist, validateBudgetInput } from "../middlewares/budget";
 import { ExpensesController } from "../controllers/ExpenseController";
-import { validateExpenseInput } from "../middlewares/Expense";
+import { validateExpenseById, validateExpenseExist, validateExpenseInput } from "../middlewares/Expense";
 
 const router = Router();
 
 router.param('budgetID', validateBudgetById);
 router.param('budgetID', validateBudgetExist);
+
+router.param('expenseID', validateExpenseById);
+router.param('expenseID', validateExpenseExist);
 
 router.get('/', BudgetController.getAll);
 
