3514ca9b242fba1f01bc2044e46f4fad2505ebd6
David <daviolivero2103@gmail.com>
2026-02-07 13:45:06 -0400

    feat(controllers): create user and verify already existed

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,24 @@
 import { Request, Response } from "express";
+import User from "../models/User";
 
 export class AuthController {
-    public static createAccount = (req: Request, res: Response) => {
-        res.json('Desde create account');
+    public static createAccount = async (req: Request, res: Response) => {
+        try {
+            const {email, password} = req.body;
+            const user = new User(req.body);
+
+            // Prevenir duplicados
+            const userExist = await User.findOne({where: {email}});
+            
+            if(userExist){
+                const error = new Error('There is already a user associated with that email address');
+                return res.status(409).json({error: error.message});
+            }
+
+            await user.save();
+            res.json('User created successfully');
+        } catch (error) {
+            res.status(500).json({error: 'There was a mistake'});
+        }
     };
 }
