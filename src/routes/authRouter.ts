0586975c3fc99895fcb521de30fdcb76a2f0fe8b
David <daviolivero2103@gmail.com>
2026-02-07 13:21:29 -0400

    feat(routes): adds validation to user registration

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,20 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { AuthController } from '../controllers/AuthController';
+import { handleInputErrors } from '../middlewares/validation';
 
 const router = Router();
 
-router.post('/create-account', AuthController.createAccount);
+router.post('/create-account', 
+    body('name')
+        .notEmpty().withMessage('The name cannot be empty'),
+    body('password')
+        .notEmpty().withMessage('The password cannot be empty')
+        .isLength({min: 8}).withMessage('The password is very short, it must have minimum of 8 characters'),
+    body('email')
+        .isEmail().withMessage('Invalid email'),
+    handleInputErrors,
+    AuthController.createAccount);
 
 
 export default router;
