63297618ad524dd7f3df5374817ca98cdd290e9f
David <daviolivero2103@gmail.com>
2026-02-08 15:31:34 -0400

    feat(routes): install express-rate-limit to limit the number of requests

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body } from 'express-validator';
 import { AuthController } from '../controllers/AuthController';
 import { handleInputErrors } from '../middlewares/validation';
+import { limiter } from '../config/limiter';
 
 const router = Router();
 
@@ -16,7 +17,12 @@ router.post('/create-account',
     handleInputErrors,
     AuthController.createAccount);
 
-router.post('/confirm-account', AuthController.confirmAccount);
+router.post('/confirm-account', 
+    limiter,
+    body('token')
+        .notEmpty().isLength({min: 6, max: 6}).withMessage('Invalid token'),
+    handleInputErrors,
+    AuthController.confirmAccount);
 
 
 export default router;
