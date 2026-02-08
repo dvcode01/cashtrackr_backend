37c7f5f1beba950e887389ccea2c3403c9bd0478
David <daviolivero2103@gmail.com>
2026-02-08 16:33:08 -0400

    chore(controllers): check if the password entered is correct

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import User from "../models/User";
-import { hashPassword } from '../utils/auth';
+import { checkPassword, hashPassword } from '../utils/auth';
 import { generateToken } from "../utils/token";
 import { AuthEmail } from "../emails/AuthEmail";
 
@@ -62,6 +62,13 @@ export class AuthController {
         if(!user.confirmed){
             const error = new Error('User not confirmed');
             return res.status(403).json({error: error.message});
+        }
+
+        const isPasswordCorrect = checkPassword(password, user.password);
+        
+        if(!isPasswordCorrect){
+            const error = new Error('Incorrect password');
+            return res.status(401).json({error: error.message});
         }
 
         res.json(user);
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,3 +4,7 @@ export const hashPassword = async (password: string) => {
     const saltPass = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, saltPass);
 }
+
+export const checkPassword = async (password: string, hash: string) => {
+    return await bcrypt.compare(password, hash);
+};
