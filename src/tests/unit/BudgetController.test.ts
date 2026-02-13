37817214c262b17e40e2c89177737c7d6d970155
David <daviolivero2103@gmail.com>
2026-02-12 21:13:33 -0400

    test: add a mock to the controller

--- src/tests/unit/BudgetController.test.ts
+++ src/tests/unit/BudgetController.test.ts
@@ -1,7 +1,26 @@
+import { createRequest, createResponse } from 'node-mocks-http';
 import { budgets } from "../mocks/budget";
+import { BudgetController } from '../../controllers/BudgetController';
+import Budget from '../../models/Budget';
+
+jest.mock('../../models/Budget', () => ({
+    findAll: jest.fn()
+}));
 
 describe('Get all budgets', () => {
-    it('Should retrieve 3 budgets', () => { 
-        expect(budgets).toHaveLength(3);
-    })
+    it('Should retrieve 3 budgets', async () => { 
+        const req = createRequest({
+            method: 'GET',
+            url: '/api/budgets',
+            user: {id: 1}
+        });
+
+        const res = createResponse();
+        
+        (Budget.findAll as jest.Mock).mockResolvedValue(budgets);
+        await BudgetController.getAll(req, res);
+        
+        const data = res._getJSONData();
+        console.log(data);
+    });
 });
