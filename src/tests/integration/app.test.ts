8b93c8c7629d4015553fccb713057f03f8a6ab99
David <daviolivero2103@gmail.com>
2026-02-25 23:27:23 -0400

    test(integration): check user password extension in registration

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -41,6 +41,31 @@ describe('Authentication - Create Account', () => {
         expect(response.body.errors).not.toHaveLength(2);
 
         expect(createAccountMock).not.toHaveBeenCalled();
+        expect(response.body.errors[0].msg).toBe('Invalid email');
+    });
+
+    it('Should return 400 status when password is less than 8 characters', async () => {
+        const user = {
+            email: 'correo@correo.com',
+            name: 'Alex',
+            password: '1234'
+        };
+        
+        const response = await request(server)
+                                    .post('/api/auth/create-account')
+                                    .send(user);
+
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount');
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+
+        expect(response.status).not.toBe(201);
+        expect(response.body.errors).not.toHaveLength(2);
+
+        expect(createAccountMock).not.toHaveBeenCalled();
+        expect(response.body.errors[0].msg).toBe('The password is very short, it must have minimum of 8 characters');
     });
 
 });
