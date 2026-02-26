991c6a769e6f644076c0a9f09eed436a7c7a697c
David <daviolivero2103@gmail.com>
2026-02-25 23:11:30 -0400

    test(integration): check when email is invalid

--- src/tests/integration/app.test.ts
+++ src/tests/integration/app.test.ts
@@ -20,5 +20,27 @@ describe('Authentication - Create Account', () => {
         expect(createAccountMock).not.toHaveBeenCalled();
     });
 
+    it('Should return 400 status when email is invalid', async () => {
+        const user = {
+            email: 'correo',
+            name: 'Alex',
+            password: '123456789'
+        };
+        
+        const response = await request(server)
+                                    .post('/api/auth/create-account')
+                                    .send(user);
+
+        const createAccountMock = jest.spyOn(AuthController, 'createAccount');
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('errors');
+        expect(response.body.errors).toHaveLength(1);
+
+        expect(response.status).not.toBe(201);
+        expect(response.body.errors).not.toHaveLength(2);
+
+        expect(createAccountMock).not.toHaveBeenCalled();
+    });
 
 });
