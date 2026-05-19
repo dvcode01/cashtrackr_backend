f45044dcd2212cdfd239d7aebe624d203928b1be
David <daviolivero2103@gmail.com>
2026-05-18 22:20:49 -0400

    feat(data): cleans test data to db

---
+++ src/data/index.ts
@@ -0,0 +1,20 @@
+import { exit } from 'node:process';
+import { db } from "../config/db";
+
+const clearData = async () => {
+    try {
+        await db.sync({force: true});
+        exit(0);
+
+        console.log('Datos eliminados correctamente');
+    } catch (error) {
+        //console.log(error);
+        exit(1)
+    }
+};
+
+if(process.argv[2] === '--clear'){
+    clearData();
+}
+
+
